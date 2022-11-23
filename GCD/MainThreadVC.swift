b087bf9caf7b91000c6ac91b9dfcad9ddcfe35af
PaulNguyen <paul.nguyen.goldenowl@gmail.com>
2022-11-23 14:31:58 +0700

    add data

--- GCD/MainThreadVC.swift
+++ GCD/MainThreadVC.swift
@@ -10,7 +10,23 @@ class MainThreadVC: UIViewController {
         return imageView
     }()
     
-    private lazy var loading = UIActivityIndicatorView()
+    private lazy var loading: UIActivityIndicatorView = {
+        let loading = UIActivityIndicatorView()
+        loading.translatesAutoresizingMaskIntoConstraints = false
+        loading.color = .red
+        loading.transform = CGAffineTransformMakeScale(1.75, 1.75)
+        return loading
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Reload", for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(fetchImage), for: .touchUpInside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,25 +39,35 @@ class MainThreadVC: UIViewController {
     
     private func setView() {
         view.addSubview(imageView)
+        view.addSubview(button)
+        view.addSubview(loading)
+        
         NSLayoutConstraint.activate([
             imageView.widthAnchor.constraint(equalToConstant: 350),
             imageView.heightAnchor.constraint(equalToConstant: 200),
             imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
+            button.widthAnchor.constraint(equalToConstant: 200),
+            
+            loading.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -36),
+            loading.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            loading.widthAnchor.constraint(equalToConstant: 100),
+            loading.heightAnchor.constraint(equalToConstant: 100)
         ])
         
-        view.addSubview(loading)
-        loading.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-        loading.center = view.center
-        loading.color = .red
         loading.startAnimating()
     }
     
+    @objc
     private func fetchImage() {
+        loading.isHidden = false
+        
         guard let url = URL(string: url) else { return }
         
         // background thread
-        
         URLSession(configuration: .default).dataTask(with: url, completionHandler: { data, _, _ in
             guard let data = data else {
                 return
